6b24490d0a174c03ec767fea443e55aabcfc18f1
SamsonGoodenough <goodenough.samson@gmail.com>
2022-04-01 23:35:58 -0400

    Added safe state and array helper functions

--- bankers-algorithm/main.c
+++ bankers-algorithm/main.c
@@ -11,6 +11,7 @@ CP386 Assignment 4 - Question 1 - Banker's Algorithm
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <pthread.h>
 #include <sys/stat.h>
@@ -27,10 +28,19 @@ struct Customer {
 } customer;
 
 // prototypes
-void printError(char *error);
-void printCustomer(struct Customer customer);
 int init_available(int argc, char *args[]);
 int init_customers();
+bool isSafe();
+
+// helper prototypes
+bool arrayLessThan(int *array1, int *array2);
+bool arrayGreaterThan(int *array1, int *array2);
+bool arrayLessThanEqual(int *array1, int *array2);
+bool arrayGreaterThanEqual(int *array1, int *array2);
+void printError(char *error);
+void printAvailable();
+void printCustomer(struct Customer customer);
+void printStatus();
 
 // global variables
 const char *FILENAME = "sample4_in.txt";
@@ -62,15 +72,12 @@ int main(int argc, char *args[]) {
       return 1;
     }
   }
-
   
   numTypes = init_available(argc, args);  // initialize available resources
   numCustomers = init_customers();        // initialize customers
   
-  // print all customers
-  for (int i = 0; i < numCustomers; i++) {
-    printCustomer(customers[i]);
-  }
+  // print status
+  printStatus();
 }
 
 // methods
@@ -183,6 +190,176 @@ int init_customers() {
 
 /*
 ----------------------------------------------------------
+Checks if the system is in a safe state
+Use: if (isSafe()) {...}
+----------------------------------------------------------
+Returns:
+  safe - [true/false] whether the system is safe or not
+----------------------------------------------------------
+*/
+bool isSafe() {
+  bool safe = true;
+
+  // ===== step 1 =====
+  // define work = available and finish = false
+  int *work = malloc(numTypes * sizeof(int));
+  bool *finish = malloc(numCustomers * sizeof(bool));
+  for (unsigned int i = 0; i < numTypes; i++) {
+    work[i] = available[i];
+    finish[i] = available[i];
+  }
+
+  // ===== step 2 =====
+  unsigned int i = 0;
+  bool checkedAll = false;
+  bool changeMade;
+  while (checkedAll == false) {
+    changeMade = false;
+
+    if (finish[i] == false) {
+      if (arrayLessThan(customers[i].need, work)) {
+        // ===== step 3 =====
+        for (unsigned int j = 0; j < numTypes; j++) {
+          work[j] += customers[i].allocated[j];
+        }
+
+        finish[i] = true;
+        changeMade = true;
+      }
+    }
+
+    i++;
+    if (i >= numCustomers) {
+      if (changeMade) {
+        i = 0;
+      } else {
+        checkedAll = true;
+      }
+    }
+  }
+
+  // ===== step 4 =====
+  for (unsigned int i = 0; i < numTypes; i++) {
+    if (finish[i] == false) {
+      safe = false;
+    }
+  }
+
+  return safe;
+}
+
+// helper methods
+
+/*
+----------------------------------------------------------
+Check if array is less than another array
+Use: if (arrayLessThan(array1, array2)) {...}
+----------------------------------------------------------
+Parameters:
+  int *array1 - first array to compare
+  int *array2 - second array to compare against
+Returns:
+  lessThan - [true/false] whether the first array is less
+    than the second array
+----------------------------------------------------------
+*/
+bool arrayLessThan(int *array1, int *array2) {
+  bool lessThan = true;
+  unsigned int i = 0;
+  while (i < numTypes && lessThan == true) {
+    if (array1[i] >= array2[i]) {
+      lessThan = false;
+    }
+
+    i++;
+  }
+
+  return lessThan;
+}
+
+/*
+----------------------------------------------------------
+Check if array is greater than another array
+Use: if (arrayGreaterThan(array1, array2)) {...}
+----------------------------------------------------------
+Parameters:
+  int *array1 - first array to compare
+  int *array2 - second array to compare against
+Returns:
+  greaterThan - [true/false] whether the first array is
+    greater than the second array
+----------------------------------------------------------
+*/
+bool arrayGreaterThan(int *array1, int *array2) {
+  bool greaterThan = true;
+  unsigned int i = 0;
+  while (i < numTypes && greaterThan == true) {
+    if (array1[i] <= array2[i]) {
+      greaterThan = false;
+    }
+
+    i++;
+  }
+
+  return greaterThan;
+}
+
+/*
+----------------------------------------------------------
+Check if array is less than or equal to another array
+Use: if (arrayLessThan(array1, array2)) {...}
+----------------------------------------------------------
+Parameters:
+  int *array1 - first array to compare
+  int *array2 - second array to compare against
+Returns:
+  lessThan - [true/false] whether the first array is less
+    than or equal to the second array
+----------------------------------------------------------
+*/
+bool arrayLessThanEqual(int *array1, int *array2) {
+  bool lessThanEqual = true;
+  unsigned int i = 0;
+  while (i < numTypes && lessThanEqual == true) {
+    if (array1[i] > array2[i]) {
+      lessThanEqual = false;
+    }
+
+    i++;
+  }
+
+  return lessThanEqual;
+}
+
+/*
+----------------------------------------------------------
+Check if array is greater than or equal to another array
+Use: if (arrayGreaterThan(array1, array2)) {...}
+----------------------------------------------------------
+Parameters:
+  int *array1 - first array to compare
+  int *array2 - second array to compare against
+Returns:
+  greaterThan - [true/false] whether the first array is
+    greater than or equal to the second array
+----------------------------------------------------------
+*/
+bool arrayGreaterThanEqual(int *array1, int *array2) {
+  bool greaterThanEqual = true;
+  unsigned int i = 0;
+  while (i < numTypes && greaterThanEqual == true) {
+    if (array1[i] < array2[i]) {
+      greaterThanEqual = false;
+    }
+
+    i++;
+  }
+
+  return greaterThanEqual;
+}
+
+/*
+----------------------------------------------------------
 Prints an error message and exits the program
 Use: printError("This is an error message");
 ----------------------------------------------------------
@@ -193,6 +370,20 @@ Parameters:
 void printError(char *error) {
   printf("Error: %s\n", error);
   exit(1);
+}
+
+/*
+----------------------------------------------------------
+Prints the current available resources
+Use: printAvaliable();
+----------------------------------------------------------
+*/
+void printAvailable() {
+  printf("Currently Available resources: ");
+  for (unsigned int i = 0; i < numTypes; i++) {
+    printf("%d ", available[i]);
+  }
+  printf("\n");
 }
 
 /*
@@ -221,4 +412,42 @@ void printCustomer(struct Customer customer) {
     printf("%d\t", customer.need[i]);
   }
   printf("\n\n");
+}
+
+/*
+----------------------------------------------------------
+Prints the current status of the system
+Use: printStatus();
+----------------------------------------------------------
+*/
+void printStatus() {
+  printf("Available Resources:\n");
+  for (unsigned int i = 0; i < numTypes; i++) {
+    printf("%d ", available[i]);
+  }
+
+  printf("\nMaximum Resources:");
+  for (unsigned int i = 0; i < numCustomers; i++) {
+    printf("\n");
+    for (unsigned int j = 0; j < numTypes; j++) {
+      printf("%d ", customers[i].maximum[j]);
+    }
+  }
+
+  printf("\nAllocated Resources:");
+  for (unsigned int i = 0; i < numCustomers; i++) {
+    printf("\n");
+    for (unsigned int j = 0; j < numTypes; j++) {
+      printf("%d ", customers[i].allocated[j]);
+    }
+  }
+
+  printf("\nNeed Resources:");
+  for (unsigned int i = 0; i < numCustomers; i++) {
+    printf("\n");
+    for (unsigned int j = 0; j < numTypes; j++) {
+      printf("%d ", customers[i].need[j]);
+    }
+  }
+  printf("\n");
 }
